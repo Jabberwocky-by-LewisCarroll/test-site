330a1821d096e6ba3a3881421731a2fcdd58c9cd
George <gogidgan95@gmail.com>
2025-07-29 15:13:33 +0300

    Multiple changes. Index.html is autorization page now. Auth don`t work yet. Registration page is created. Multiple visual changes.

--- src/scripts/reg.js
+++ src/scripts/reg.js
@@ -6,23 +6,23 @@ document.addEventListener('DOMContentLoaded', function () {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData.entries());
 
-        fetch('https://api.directual.com/good/api/v5/auth?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b', {
+        fetch('https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-
-            // Для теста временно логин и пароль заменяют email и name из объектов
-            body: JSON.stringify({
-                "provider": "rest",
-                "username": data.email,
-                "password": data.name,
-            })
+            body: JSON.stringify(data)
         })
+
+         // Обработка ответа от сервера
         .then(response => response.json())
         .then(result => {
-            // Обработка ответа от сервера
             alert('Ответ сервера: ' + JSON.stringify(result));
+
+            // Если регистрация успешна, перенаправляем на страницу авторизации
+            if (result.status === "OK") {
+                window.location.href = '../index.html';
+            }
         })
         .catch(error => {
             alert('Ошибка! Ответ сервера: ' + JSON.stringify(error));
