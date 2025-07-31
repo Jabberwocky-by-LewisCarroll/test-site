cb9ac93f55a1258b8c1d3dcee585a206b5a8f428
George <gogidgan95@gmail.com>
2025-08-01 01:28:42 +0300

    Reg logic fixed again

--- src/scripts/reg.js
+++ src/scripts/reg.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
             alert('Регистрация прошла успешно! Ответ сервера: ' + JSON.stringify(result));
 
             // Если регистрация успешна, перенаправляем на страницу авторизации
-            if (result.result.success === true) {
+            if (result.result[0].success === true) {
                 window.location.href = '../index.html';
             }
             else {
