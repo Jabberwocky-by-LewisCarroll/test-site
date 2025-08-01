38423b43c3717e3ac959818af244ac6aedf7d7fd
George <gogidgan95@gmail.com>
2025-08-01 16:05:35 +0300

    Delete obj card logic added

--- src/scripts/getObjectsV2.js
+++ src/scripts/getObjectsV2.js
@@ -5,11 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
         return;
     }
 
+    // Сохраняем appID и sessionID для дальнейшего использования
+    const appID = '640a9d7b-5015-4ea0-8206-a4eddcef054b';
+    const sessionID = localStorage.getItem('sessionID');
+
+    // Контейнер с карточками
     const container = document.querySelector('.obj-wrapper');
-    // Получаем шаблон из контейнера
+    // Шаблон карточки в контейнере
     const template = container.querySelector('template');
 
-    fetch('https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=640a9d7b-5015-4ea0-8206-a4eddcef054b&sessionID=')
+    fetch(`https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=${appID}&sessionID=${sessionID}`)
         .then(response => response.json())
         .then(data => {
             const objects = Array.isArray(data.payload) ? data.payload : [];
@@ -17,14 +22,57 @@ document.addEventListener('DOMContentLoaded', function () {
                 objects.forEach(obj => {
                     // Клонируем содержимое шаблона
                     const clone = template.content.cloneNode(true);
-                    clone.querySelector('.name').textContent = obj.name || '';
-                    clone.querySelector('.surname').textContent = obj.surname || '';
-                    clone.querySelector('.email').textContent = obj.email || '';
-                    clone.querySelector('.id').textContent = obj.id || '';
-                    clone.querySelector('#btnView').href = `detailedInfo.html?id=${obj.id}`;
-                    // clone.querySelector('#btnChange').href = '';
-                    // clone.querySelector('#btnDelete').href = '';
-                    container.appendChild(clone);
+                    const card = clone.querySelector('.obj-card');
+                    card.querySelector('.name').textContent = obj.name || '';
+                    card.querySelector('.surname').textContent = obj.surname || '';
+                    card.querySelector('.email').textContent = obj.email || '';
+                    card.querySelector('.id').textContent = obj.id || '';
+                    card.querySelector('.link-btn.view').href = `detailedInfo.html?id=${obj.id}`;
+                    card.querySelector('.link-btn.change');
+                    card.querySelector('.link-btn.delete');
+                    container.appendChild(card);
+
+                    // Обработка кнопки "Удалить"
+                    const btnDelete = card.querySelector('.link-btn.delete');
+
+                    // Проверка на наличие кнопки "Удалить"
+                    if (btnDelete) {
+                        btnDelete.addEventListener('click', function(e) {
+                            e.preventDefault();
+
+                            const name = card.querySelector('.name').textContent;
+                            const surname = card.querySelector('.surname').textContent;
+                            const email = card.querySelector('.email').textContent;
+                            const id = card.querySelector('.id').textContent;
+
+                            fetch(`https://api.directual.com/good/api/v5/data/inputtestapi/editTestObjects?appID=${appID}&sessionID=${sessionID}`, {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({
+                                    "name": name,
+                                    "surname": surname,
+                                    "email": email,
+                                    "testApiStructID": id,
+                                    "action": "DELETE"
+                                })
+                            })
+                            .then(response => response.json())
+                            .then(result => {
+                                if (result.result[0].success === true) {
+                                    alert(`Объект '${id}' успешно удалён`)
+                                    card.remove();
+                                }
+                                else {
+                                    alert(`Ошибка удаления на сервере:\n\n${result.result[0].Result}`)
+                                }
+                                })
+                            .catch(error => {
+                                console.log(error);
+                            })
+                        })
+                    }
                 });
             } else {
                 container.innerHTML += '<p>Нет данных</p>';
