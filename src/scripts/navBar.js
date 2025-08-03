79342364e052b670351218ac7b6ead77b3c83183
George <gogidgan95@gmail.com>
2025-08-03 21:26:54 +0300

    NavBar is generating automaticly now. LogOutBtn and disableCurrentPageBtn logic are part of navBar.js. logOutBtn.js and currentPageBtnDisabled.js deleted.

---
+++ src/scripts/navBar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navBar = document.querySelector('#navbar');
+
+    // Скрипт влияет на кнопку в навигации, которая ведёт на текущую страницу.
+    // Если кнопка ведёт на текущую страницу, то она будет неактивна.
+    function disableCurrentPageLink() {
+        const currentPage = location.pathname.split('/').pop();
+        const btn = document.querySelector(`a[href="${currentPage}"].link-btn`);
+
+        if (btn && btn.getAttribute('href') === currentPage) {
+            btn.setAttribute('aria-disabled', 'true');
+        }
+    };
+
+    // Кнопка для выхода их аккаунта
+    // Удаляет sessionID из localStorage и перенаправляет на страницу авторизации
+    function logOutBtn () {
+        const logoutBtn = document.querySelector('#logoutBtn');
+        if (logoutBtn) {
+            logoutBtn.addEventListener('click', function () {
+                window.localStorage.removeItem('sessionID');
+                alert('sessionID deleted!');
+                window.location.href = logoutBtn.href;
+            })
+        };
+    };
+
+    // Генерация навбара
+    if (navBar) {
+        fetch('../public/navBar.html')
+        .then(response => response.text())
+        .then(html => {
+            navBar.innerHTML = html;
+            disableCurrentPageLink(); // Вызов функции для отключения кнопки текущей страницы
+            logOutBtn();
+        })
+        .catch(error => {
+            console.log('Ошибка генерации навбара: ', error)
+        });
+    };
+});
