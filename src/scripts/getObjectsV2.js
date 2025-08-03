bbf1e5589dad9f993ddf56136ce53f8fe03f3587
George <gogidgan95@gmail.com>
2025-08-03 16:01:58 +0300

    Add form sending and new card creating added

--- src/scripts/getObjectsV2.js
+++ src/scripts/getObjectsV2.js
@@ -50,17 +50,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     form.elements['id'].value = obj.id || '';
                 });
 
-                // Обработчик кнопки "Закрыть форму"
-                const btnFormExit = document.querySelectorAll('.btn__exit-form')
-                btnFormExit.forEach(btn => {
-                    btn.addEventListener('click', function(e) {
-                        e.preventDefault();
-                        const popup = document.querySelectorAll('.popup-window');
-                        popup.forEach(p => p.classList.replace('show', 'hide'));
-                    });
-                });
-
-                const btnDelete = card.querySelector('.link-btn.delete'); // Обработка кнопки "Удалить"
+                // Обработка кнопки "Удалить"
+                const btnDelete = card.querySelector('.link-btn.delete');
                 
                 if (btnDelete) { // Проверка на наличие кнопки "Удалить"
                     btnDelete.addEventListener('click', function(e) {
@@ -100,58 +91,6 @@ document.addEventListener('DOMContentLoaded', function () {
                     })
                 };
             });
-            
-            const btnFormAdd = document.querySelector('button[type="button"].link-btn.add'); // Обработчик кнопки "Добавить"
-            btnFormAdd.addEventListener('click', function() {
-                const popup = document.querySelector('div.popup-window.add');
-                popup.classList.replace('hide', 'show');
-            });
-
-            // Обработчик формы редактирования карточек
-            const formEdit = document.querySelector('form.edit');
-            formEdit.addEventListener('submit', function(e) {
-                e.preventDefault();
-
-                const name = formEdit.elements['name'].value;
-                const surname = formEdit.elements['surname'].value;
-                const email = formEdit.elements['email'].value;
-                const id = formEdit.elements['id'].value;
-
-                fetch(`https://api.directual.com/good/api/v5/data/inputtestapi/editTestObjects?appID=${appID}&sessionID=${sessionID}`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        "name": name,
-                        "surname": surname,
-                        "email": email,
-                        "testApiStructID": id,
-                        "action": "UPDATE"
-                    })
-                })
-                .then(response => response.json())
-                .then(result => {
-                    if(result.result[0].success === true) {
-                        alert('Данные успешно обновлены');
-                        const popup = document.querySelector('div.popup-window.edit');
-                        popup.classList.replace('show', 'hide');
-
-                        // --- обновляем карточку на странице ---
-                        if (currentEditingCard) {
-                            currentEditingCard.querySelector('.name').textContent = name;
-                            currentEditingCard.querySelector('.surname').textContent = surname;
-                            currentEditingCard.querySelector('.email').textContent = email;
-                            currentEditingCard.querySelector('.id').textContent = id;
-                        }
-                    } else {
-                        alert('Ошибка обновления: ' + result.result[0].Result);
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-            })
         } else {
             container.innerHTML += '<p>Нет данных</p>';
         }
@@ -159,5 +98,109 @@ document.addEventListener('DOMContentLoaded', function () {
     .catch(error => {
         container.innerHTML += '<p>Ошибка загрузки данных</p>';
         console.error(error);
+    });
+
+    // Сюда надо вынести все обработчики, которые не зависят от генерации карточек
+            
+    // Обработчик кнопки "Добавить"
+    const btnFormAdd = document.querySelector('button[type="button"].link-btn.add');
+    btnFormAdd.addEventListener('click', function() {
+        const popup = document.querySelector('div.popup-window.add');
+        popup.classList.replace('hide', 'show');
+    });
+
+    // Обработчик формы добавления карточек
+    const formAdd = document.querySelector('form.add');
+    formAdd.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const name = formAdd.elements['name'].value;
+        const surname = formAdd.elements['surname'].value;
+        const email = formAdd.elements['email'].value;
+
+        fetch(`https://api.directual.com/good/api/v5/data/testApi/newTestUser?appID=${appID}&sessionID=${sessionID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": name,
+                "surname": surname,
+                "email": email
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            alert('Карточка объекта успешно добавлена!');
+            const popup = document.querySelector('div.popup-window.add');
+            popup.classList.replace('show', 'hide');
+
+            const clone = template.content.cloneNode(true);
+            const card = clone.querySelector('.obj-card');
+            card.querySelector('.name').textContent = result.result[0].name;
+            card.querySelector('.surname').textContent = result.result[0].surname;
+            card.querySelector('.email').textContent = result.result[0].email;
+            card.querySelector('.id').textContent = result.result[0].id;
+            container.appendChild(card);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    })
+
+    // Обработчик формы редактирования карточек
+    const formEdit = document.querySelector('form.edit');
+    formEdit.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const name = formEdit.elements['name'].value;
+        const surname = formEdit.elements['surname'].value;
+        const email = formEdit.elements['email'].value;
+        const id = formEdit.elements['id'].value;
+
+        fetch(`https://api.directual.com/good/api/v5/data/inputtestapi/editTestObjects?appID=${appID}&sessionID=${sessionID}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": name,
+                "surname": surname,
+                "email": email,
+                "testApiStructID": id,
+                "action": "UPDATE"
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            if(result.result[0].success === true) {
+                alert('Данные успешно обновлены');
+                const popup = document.querySelector('div.popup-window.edit');
+                popup.classList.replace('show', 'hide');
+
+                // --- обновляем карточку на странице ---
+                if (currentEditingCard) {
+                    currentEditingCard.querySelector('.name').textContent = name;
+                    currentEditingCard.querySelector('.surname').textContent = surname;
+                    currentEditingCard.querySelector('.email').textContent = email;
+                    currentEditingCard.querySelector('.id').textContent = id;
+                }
+            } else {
+                alert('Ошибка обновления: ' + result.result[0].Result);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    });
+
+    // Обработчик кнопки "Закрыть форму"
+    const btnFormExit = document.querySelectorAll('.btn__exit-form')
+    btnFormExit.forEach(btn => {
+        btn.addEventListener('click', function(e) {
+            e.preventDefault();
+            const popup = document.querySelectorAll('.popup-window');
+            popup.forEach(p => p.classList.replace('show', 'hide'));
+        });
     });
 });
