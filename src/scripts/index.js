49945c44be6ed1a4a697e34f5edd2b1670a473a6
George <gogidgan95@gmail.com>
2025-08-07 15:22:51 +0300

    Made cards more fluid adaptive. Some comments added.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -80,27 +80,37 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     // Анимация drag-and-drop для тестовых данных
+    // Получаем все элементы, которые можем перетаскивать
     const draggables = document.querySelectorAll('div.test-data__input-draggable');
+    // Получаем поля для ввода логина и пароля
     const loginInput = document.querySelector('input[name="login"]');
     const passwordInput = document.querySelector('input[name="password"]');
+    // Переменная для хранения текущего перетаскиваемого элемента
     let isDraggable = null;
+    // Начальные координаты мыши
     let startMouseX = 0;
     let startMouseY = 0;
     
+    // С помощью метода массивов .forEach() добавляем обработчик события mousedown для каждого
+    // перетаскиваемого элемента. При нажатии происходит сохранение координат мыши, а в переменную
+    // isDraggable сохраняется перетаскиваемый элемент.
     draggables.forEach(item => {
         item.addEventListener('mousedown', function(e) {
             isDraggable = item;
             startMouseX = e.clientX;
             startMouseY = e.clientY;
-            document.body.style.userSelect = 'none';
+            document.body.style.userSelect = 'none'; // Отмена функции выделения текста
         });
     });
 
+    // Обработчик события передвижения мыши. С помощью переменной isDraggable проверяется
+    // нажата ли кнопка мыши на элементе в данный момент. 
     document.addEventListener('mousemove', function(e) {
-        if (!isDraggable) return;
-        const dx = e.clientX - startMouseX;
-        const dy = e.clientY - startMouseY;
-        isDraggable.style.transform = `translate(${dx}px, ${dy}px)`;
+        if (isDraggable) {
+            const dx = e.clientX - startMouseX;
+            const dy = e.clientY - startMouseY;
+            isDraggable.style.transform = `translate(${dx}px, ${dy}px)`;
+        } else { return };
     });
 
     document.addEventListener('mouseup', function(e) {
